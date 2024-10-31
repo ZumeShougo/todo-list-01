a914934be64a36fbacbc872773d355e18749bf8b
ZumeShougo <88694354+ZumeShougo@users.noreply.github.com>
2024-10-31 18:00:20 +0900

    fix

--- common.js
+++ common.js
@@ -1,14 +1,17 @@
 function addTask() {
+    // inputタグの内容を変数に格納
     const taskInput = document.getElementById("taskInput");
+    // ul要素を格納
     const taskList = document.getElementById("taskList");
 
     // 入力が空でなければ処理を進める
     if (taskInput.value.trim() !== "") {
-        // 新しいタスク項目を作成
+        // 新しいHTML要素を作成(liを作成)
         const newTask = document.createElement("li");
 
         // タスク内容のテキストノード
         const taskText = document.createTextNode(taskInput.value);
+        // インプットの内容を新しく生成したliの中に入れる
         newTask.appendChild(taskText);
 
         // チェックボックスを追加して完了機能を実装
